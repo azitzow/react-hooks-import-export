45397f3a36809e446ae877e1d02832d521e0660c
Ana Zitzow <anasempai89@gmail.com>
2022-05-19 11:15:57 -0700

    commit tree 1c3f91b72e4e7c3393f1d3ddd58b0e2fddf81eaa

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,11 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { trees, wildlife } from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks;
